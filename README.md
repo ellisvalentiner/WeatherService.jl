62b9cb1a04f6e74074e56375f066255789fe5d09
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-05-02 10:44:34 -0400

    MVP

--- README.md
+++ README.md
@@ -3,5 +3,37 @@
 ***REMOVED***
 
 ***REMOVED***
-julia app.jl
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+Sample response:
+
+***REMOVED***json
+{
+  "visibility": 10,
+  "apparentTemperature": 72.55,
+  "time": 1525272163,
+  "uvIndex": 3,
+  "precipIntensity": 0,
+  "dewPoint": 47.5,
+  "precipProbability": 0,
+  "temperature": 72.55,
+  "windBearing": 221,
+  "icon": "wind",
+  "nearestStormBearing": 132,
+  "cloudCover": 0.5,
+  "summary": "Breezy and Partly Cloudy",
+  "pressure": 1014.76,
+  "windGust": 18.85,
+  "ozone": 344.21,
+  "windSpeed": 18.38,
+  "humidity": 0.41,
+  "nearestStormDistance": 11
+}
 ***REMOVED***
--- app.jl
+++ app.jl
@@ -1,16 +1,23 @@
 using GoogleMaps
 using DarkSky
 using HttpServer
+using HttpCommon
+using JSON
 
 http = HttpHandler() do req::Request, res::Response
-    m = match(r"^/zipcode/\d{5}/?$",req.resource)
+    m = match(r"^/zipcode/(\d+)/?$",req.resource)
     if m == nothing return Response(404) end
-    input = String(m.captures[1])
-    response = geocode(input)
-    latlng = response["results"][1]["geometry"]["location"]
+    input = String(m.captures[1]);
+    response = geocode(input);
+    latlng = response["results"][1]["geometry"]["location"];
     response = forecast(latlng["lat"], latlng["lng"]);
-    return Response("$(currently(response))")
+    x = JSON.json(response.currently)
+    resp = Response(x)
+    resp.headers["Content-Type"] = "application/json"
+    return resp
 end
+http.events["error"]  = (client, err) -> println(err)
+http.events["listen"] = (port)        -> println("Listening on $port...")
 
 server = Server( http )
 run( server, 8000 )
